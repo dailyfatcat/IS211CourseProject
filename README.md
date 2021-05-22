e985b737cf5a1b82e43e3d0674d407134ee2b081
dailyfatcat <50534920+dailyfatcat@users.noreply.github.com>
2021-05-22 18:46:51 -0400

    Project

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 <h1><strong>Project Option 2: Blog Application </strong></h1>
 
 <h2>Database</h2> The database has three tables, there is a table for the blog posts and a table for authors. 
-Since there are many authors that can make many blog posts the data is normalized and there is a third table containing the post id and author ids.
-The db.py file is used to create the database where the schema is defined in schema.sql.
+Since many authors can make many blog posts the data is normalized and there is a third table containing the post id and author ids, this third table normalizes the data by removing repetitive lines.
+The db.py file is used to create the database where the schema is defined in schema.sql. The key is automatically created and auto-incremented.
 
 <h3>Creating the database</h3>
-createdb.py creates the initial database and adds two example enteries outside of the application.
-  
+createdb.py creates the initial database and adds two example entries outside of the application. The database is called "blogs.db"
+
+<h2> The Blog Application</h2>
+The blog application requires that user logs in, using the session to keep track that the user has logged in to edit the blog posts, delete the blog posts or create new blog posts.
+<h3>Login Information</h3>
+<strong>username:</strong> sam
+<strong>password:</strong> password
+Since the project is created for a single user, there is no tracking of multiple users, usernames, or passwords but rather that the user has logged in with the username ‘sam and the password ‘password’.
+<h3>Blog Application</h3>
+The index page, or main page, queries the database and lists all the posts that a user can view, the user can view the posts here from the links. 
+Once the user is logged in, the dashboard page ensures the user is logged in before querying the database to get all the blog posts then render the dashboard template. This is where the user can then choose to edit, delete, or view current blog posts or create a new blog post. The button is clicked to complete the actions. </br>
+Every blog post has an auto-incremented key id that is used to find the blog post with a query for editing and deleting. 
--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def get_db():
 
 @app.route('/')
 def index():
-    ''''''
+    '''Index page'''
     db = get_db()
     blog_posts = g.db.execute("SELECT posts.title, posts.pub_date, posts.content, authors.author_name, posts.post_id FROM created "
                                "INNER JOIN posts on created.post_id == posts.post_id "
@@ -72,8 +72,8 @@ def updatepost():
     postID = request.form.get('postID')
     author = 1
     db = get_db()
-    db.execute("UPDATE posts SET title = ?, content = ? WHERE post_id =?", (title, content, postID), )
-    db.commit()
+    g.db.execute("UPDATE posts SET title = ?, content = ? WHERE post_id =?", (title, content, postID), )
+    g.db.commit()
     return (redirect(url_for("dashboard")))
 
 
@@ -85,11 +85,9 @@ def add():
     content = request.form.get('content')
     author = 1
     db = get_db()
-    postID = db.execute("INSERT INTO posts (title, content) VALUES (?,?)", (title, content,)).lastrowid
-
-
-    db.execute("INSERT INTO created (author_id, post_id) VALUES (?,?)", (author, postID, ))
-    db.commit()
+    postID = g.db.execute("INSERT INTO posts (title, content) VALUES (?,?)", (title, content,)).lastrowid
+    g.db.execute("INSERT INTO created (author_id, post_id) VALUES (?,?)", (author, postID, ))
+    g.db.commit()
     return (redirect(url_for("dashboard")))
 
 
@@ -98,21 +96,20 @@ def delPost(postID):
     if not session.get('username'):
         return (redirect(url_for("login")))
     db = get_db()
-    db.execute("DELETE FROM posts WHERE post_id =?", (postID,))
-    db.execute("DELETE FROM created WHERE post_id =?", (postID,))
-    db.commit()
+    g.db.execute("DELETE FROM posts WHERE post_id =?", (postID,))
+    g.db.execute("DELETE FROM created WHERE post_id =?", (postID,))
+    g.db.commit()
     return (redirect(url_for("dashboard")))
 
 
 @app.route('/<int:postID>')
 def post(postID):
-    if not session.get('username'):
-        return (redirect(url_for("login")))
     db = get_db()
     blog = []
+
     post = g.db.execute("SELECT * FROM posts WHERE post_id =?", (postID,)).fetchone()
     author = g.db.execute("SELECT authors.author_id, authors.author_name FROM created INNER JOIN authors on created.author_id == authors.author_id WHERE post_id =?", (postID,)).fetchone()
-    page = Markup("<h1>" + post[1] + "</h1><p>Created On: " + post[2] + "</p><p>Written By: " + author[1] + "</p><br>" + post[3])
+    page = Markup('<head><link rel="stylesheet" href="static/styles.css"><meta charset="UTF-8"></head>'+ "<body><h1>" + post[1] + "</h1><p>Created On: " + post[2] + "</p><p>Written By: " + author[1] + "</p><br>" + post[3] + "</body>")
     return render_template('post.html', post=page)
 
 
@@ -122,22 +119,22 @@ def login():
     return(render_template("login.html"))
 
 
-@app.route('/validate', methods=['POST', 'GET'])
-def validate():
-    username = request.form['username']
-    password = request.form['password']
-    if username == "admin" and password == "password":
-        session['username'] = username
-        return(redirect(url_for("dashboard")))
-    else:
-        return(redirect(url_for("login")))
-
-
 @app.route('/create_blog', methods=['GET', 'POST'])
 def create_blog():
     if not session.get('username'):
         return (redirect(url_for("login")))
     return render_template('create_blog.html')
+
+
+@app.route('/validate', methods=['POST', 'GET'])
+def validate():
+    username = request.form['username']
+    password = request.form['password']
+    if username == "sam" and password == "password":
+        session['username'] = username
+        return(redirect(url_for("dashboard")))
+    else:
+        return(redirect(url_for("login")))
 
 
 if __name__ == '__main__':
