375b94e19f1c8180f2d7803b32a712179097e81c
dailyfatcat <50534920+dailyfatcat@users.noreply.github.com>
2021-05-22 16:14:57 -0400

    Project

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask
 from flask import current_app, g, render_template
 from flask import current_app, g
 import sqlite3
+from flask import Markup
+import sqlite3
+import click
+from flask import Flask, redirect, session, url_for, request, flash
+from markupsafe import escape
+from flask import current_app, g, render_template
+from flask.cli import with_appcontext
+from db import get_db, init_db
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
@@ -19,6 +27,15 @@ def get_db():
     return g.db
 
 
+@app.route('/')
+def index():
+    ''''''
+    db = get_db()
+    blog_posts = g.db.execute("SELECT posts.title, posts.pub_date, posts.content, authors.author_name, posts.post_id FROM created "
+                               "INNER JOIN posts on created.post_id == posts.post_id "
+                               "INNER JOIN authors on created.author_id == authors.author_id ").fetchall()
+    return render_template('index.html', blogs=blog_posts)
+
 @app.route('/dashboard', methods=["POST", "GET"])
 def dashboard():
     '''Part III Dashboard: View students and quizzes in the class'''
@@ -29,9 +46,53 @@ def dashboard():
     return render_template('dashboard.html', blogs=blog_posts)
 
 
-@app.route('/edit', methods=["POST", "GET"])
-def edit():
-    pass
+@app.route('/edit/<int:postID>')
+def edit(postID):
+    db = get_db()
+    post = g.db.execute("SELECT posts.title, posts.pub_date, posts.content, authors.author_name, posts.post_id, authors.author_id FROM created "
+                               "INNER JOIN posts on created.post_id == posts.post_id "
+                               "INNER JOIN authors on created.author_id == authors.author_id WHERE created.post_id = ?", (postID,)).fetchone()
+    return render_template ('edit.html', post=post)
+
+@app.route('/update', methods=["POST", "Get"])
+def updatepost():
+    title = request.form.get('title')
+    content = request.form.get('content')
+    author = 1
+    db = get_db()
+    db.execute("INSERT INTO posts (title, content) VALUES (?,?)", (title, content,))
+    db.commit()
+    return (redirect(url_for("dashboard")))
+
+
+@app.route('/add', methods=["POST", "Get"])
+def add():
+    title = request.form.get('title')
+    content = request.form.get('content')
+    author = 1
+    db = get_db()
+    postID = db.execute("INSERT INTO posts (title, content) VALUES (?,?)", (title, content,)).lastrowid
+
+
+    db.execute("INSERT INTO created (author_id, post_id) VALUES (?,?)", (author, postID, ))
+    db.commit()
+    return (redirect(url_for("dashboard")))
+
+@app.route('/delete/<int:postID>')
+def delPost(postID):
+    db = get_db()
+    db.execute("DELETE FROM posts WHERE post_id =?", (postID,))
+    db.execute("DELETE FROM created WHERE post_id =?", (postID,))
+    db.commit()
+    return (redirect(url_for("dashboard")))
+
+@app.route('/<int:postID>')
+def post(postID):
+    db = get_db()
+    blog = []
+    post = g.db.execute("SELECT * FROM posts WHERE post_id =?", (postID,)).fetchone()
+    page = Markup("<h1>" + post[1] + "</h1><p>created: " + post[2] + "</p><p>Written By: " + "" + "</p><br>" + post[3])
+    return render_template('post.html', post=page)
 
 @app.route('/delete', methods=["POST", "GET"])
 def delete():
@@ -42,6 +103,9 @@ def login():
     '''Show the login page'''
     return(render_template("login.html"))
 
+@app.route('/create_blog', methods=['GET', 'POST'])
+def create_blog():
+    return render_template('create_blog.html')
 
 if __name__ == '__main__':
     app.run()
